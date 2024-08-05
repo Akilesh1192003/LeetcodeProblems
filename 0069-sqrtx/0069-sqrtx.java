e5cf2202976b9e5855bd935cf1bb28f9869e0970
Akilesh1192003 <143526471+Akilesh1192003@users.noreply.github.com>
2024-08-05 11:58:02 +0530

    Time: 1 ms (85.94%), Space: 40.6 MB (77.78%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,21 +1,27 @@
 class Solution {
     public int mySqrt(int x) {
-        int c = 0;
-
         if (x == 0 || x == 1) {
-            c = x;
-        } else {
-            for (int j = 1; j <= x; j++) {
-                if (j * j == x) {
-                    c = j;
-                    break;
-                } else if (j * j > x) {
-                    c = j - 1;
-                    break;
-                }
+            return x;
+        }
+
+        int left = 1, right = x, result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Check if mid*mid is equal to x
+            if (mid == x / mid) {
+                return mid;
+            }
+
+            // If mid*mid is less than x, discard the left half
+            if (mid < x / mid) {
+                left = mid + 1;
+                result = mid;  // Update result as we need floor value
+            } else {  // If mid*mid is more than x, discard the right half
+                right = mid - 1;
             }
         }
 
-        return c;
-        }
-        }
+        return result;
+        }}
