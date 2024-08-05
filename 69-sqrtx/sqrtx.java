e1e021d4ad5086d0115df06db1abe246c21e0be3
Akilesh1192003 <143526471+Akilesh1192003@users.noreply.github.com>
2024-08-05 12:12:05 +0530

    Time: 1 ms (85.94%) | Memory: 40.7 MB (62.56%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -3,22 +3,16 @@ class Solution {
         if (x == 0 || x == 1) {
             return x;
         }
-
         int left = 1, right = x, result = 0;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
-            // Check if mid*mid is equal to x
             if (mid == x / mid) {
                 return mid;
             }
-
-            // If mid*mid is less than x, discard the left half
             if (mid < x / mid) {
                 left = mid + 1;
-                result = mid;  // Update result as we need floor value
-            } else {  // If mid*mid is more than x, discard the right half
+                result = mid;  
+            } else {  
                 right = mid - 1;
             }
         }
