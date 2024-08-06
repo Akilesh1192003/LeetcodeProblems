5120d43fd7082c153f88396b1916441a24e2d358
Akilesh1192003 <143526471+Akilesh1192003@users.noreply.github.com>
2024-08-06 12:04:38 +0530

    Time: 8 ms (51.12%), Space: 41 MB (5.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public int fib(int n) {
+        if(n==0||n==1){return n;}
         return fibonacci(n-1)+fibonacci(n-2);
     }
     public int fibonacci(int num){
         int n=0;
         if(num==0||num==1){return num;}
-        else{
-           n=fibonacci(num-1)+fibonacci(num-2);
-        }
-        return n;
+           return fibonacci(num-1)+fibonacci(num-2);
     }
 }
