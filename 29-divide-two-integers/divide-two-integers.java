1c7afc9322c345048dcb909ea9ff708eebdcfc95
Akilesh1192003 <143526471+Akilesh1192003@users.noreply.github.com>
2024-08-14 18:44:38 +0530

    Time: 1 ms (71.86%) | Memory: 40.8 MB (42.43%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -4,9 +4,6 @@ class Solution {
          if (dividend == Integer.MIN_VALUE && divisor == -1) {
             return Integer.MAX_VALUE;
         }
-        if(divisor<0 && dividend <0 &&b<0){
-           b=-1*b;
-        }
         return b;
     }
 }
