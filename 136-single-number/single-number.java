96d89bc8564e1468a9e31ca0a560901847e287a4
Akilesh1192003 <143526471+Akilesh1192003@users.noreply.github.com>
2024-08-19 11:59:19 +0530

    Time: 1 ms (99.83%) | Memory: 46.3 MB (22.54%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums)
-    {
+    { int n=nums.length;
         int b=0;
-        for(int i=0;i<nums.length;i++)
+        for(int i=0;i<n;i++)
         {
             b=b^nums[i];
         }
