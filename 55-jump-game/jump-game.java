628149141646e2ab083c65c7e8701b00a72c4297
Akilesh1192003 <143526471+Akilesh1192003@users.noreply.github.com>
2024-08-19 19:42:11 +0530

    Time: 3 ms (50.81%) | Memory: 45.4 MB (69.59%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,16 +1,13 @@
-class Solution 
-{
-    public boolean canJump(int[] nums) 
-    {
+class Solution {
+    public boolean canJump(int[] nums) {
         int n=nums.length;
-        int i,m=0;
-        for(i=0;i<=n-1;i++){
+        int m=0;
+        for(int i=0;i<=n-1;i++){
             if(i>m){
                 return false;
             }
             m=Math.max(m,i+nums[i]);
         }
         return true;
-        }
-
+    }
 }
