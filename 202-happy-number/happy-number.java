d7762e93fcdb3ba41ee644f7e8243d2cb8a99c2b
Akilesh1192003 <143526471+Akilesh1192003@users.noreply.github.com>
2024-08-24 14:39:15 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (48.39%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int result(int num)
+    { int answer=0;
+        while(num>0)
+        { int res=num %10;
+           answer=answer+(int)Math.pow(res,2);
+           num=num/10;
+        }
+        return answer;
+    }
+    public boolean isHappy(int n)
+    {
+        int ans=0;
+        if(n==1)
+        {
+            return true;
+        }
+       /* if(n>1 && n<=9)
+        {
+            return false;
+        }*/
+        while(n!=1 && n>6)
+        {
+         n=result(n);
+        }
+        if(n==1){
+            return true;
+        }
+        return false;
+    }
+}
